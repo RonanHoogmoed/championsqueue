a50d14f3ff29d3aff916cfb047f34cf4285f4846
Ronan Hoogmoed <ronanhoogmoed@hotmail.com>
2022-02-28 22:37:44 +0100

    Removed manual player pages, mobile optimalisation

--- playerpage.php
+++ playerpage.php
@@ -27,8 +27,8 @@
                 <h1 class="text-center mb-5"><?php echo $playername; ?></h1>
                 <div class="mb-5">
                     <h3 class="text-center">General statistics</h3>
-                    <div class="row">
-                        <div class="col-12 col-md-6 col-lg-4 h-100">
+                    <div class="row mt-4">
+                        <div class="col-12 col-md-6 col-lg-4 h-100 mb-3">
                             <div class="bg-light">
                                 <div class="p-3 text-center">
                                     <h4>Wins</h4>
@@ -36,7 +36,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6 col-lg-4 h-100">
+                        <div class="col-12 col-md-6 col-lg-4 h-100 mb-3">
                             <div class="bg-light">
                                 <div class="p-3 text-center">
                                     <h4>Losses</h4>
@@ -44,7 +44,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6 col-lg-4 h-100">
+                        <div class="col-12 col-md-6 col-lg-4 h-100 mb-3">
                             <div class="bg-light">
                                 <div class="p-3 text-center">
                                     <h4>Win %</h4>
